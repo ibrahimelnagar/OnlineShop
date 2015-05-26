631fb12bb1ba900cfaf7d7d356c3faaf981d6ced
uczenn <tmkzmu@gmail.com>
2015-05-26 17:11:32 +0200

    Deleted error alert

--- OnlineShop/Content/js/home-controller.js
+++ OnlineShop/Content/js/home-controller.js
@@ -1,11 +1,9 @@
-﻿angular.module('shopApp', ['ngRoute'])
+﻿// ReSharper disable once PossiblyUnassignedProperty
+angular.module('shopApp', ['ngRoute'])
     .controller("HomeCtrl", function ($scope, $http) {
         $http.post('/Home/GetTopSellingAlbumsAsync')
             .success(function (data) {
                 $scope.albums = data;
-            })
-            .error(function () {
-                alert("error");
             });
     });
 
